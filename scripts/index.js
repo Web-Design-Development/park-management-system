5c8d8a6e214fef12e57d327d78fd4487b7817313
aitchIV <uchennavictor248@gmail.com>
2022-10-24 01:44:37 +0100

    second commit: more styling and javaScript

--- scripts/index.js
+++ scripts/index.js
@@ -1,76 +1,102 @@
 let form = document.querySelector('form');
 let ownerInput = document.querySelector('#owner');
 let carInput = document.querySelector('#car');
+let vehicleClass = document.querySelector('#class');
 let licenseInput = document.querySelector('#license');
 let entryInput = document.querySelector('#entry');
 let addCar = document.querySelector('#add-car')
+let carParkSedan = document.querySelector('.car-log-sedan');
+let carParkSuv = document.querySelector('.car-log-suv')
 
+// Input validation
 let owner;
 let car;
 let license;
 let entry;
 let tableRow;
+let rowLengthSedan = [];
+let rowLengthSuv = [];
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
-    if (!ownerInput.value) {
-        alert('fill owner')
-    } else {
-        const carPark = document.querySelector('.car-log');
-        tableRow = document.createElement('tr');
-        carPark.appendChild(tableRow)
-        const onwerDisplay = document.createElement('td');
-        onwerDisplay.innerText = ownerInput.value;
+    if (!ownerInput.value || !carInput.value || !vehicleClass.value || !licenseInput.value || !entryInput.value) {
+        alert('fill all blanks');
+        if (!ownerInput.value) {
+            ownerInput.classList.add('errorClass');
+        }
+        if (!carInput.value) {
+            carInput.classList.add('errorClass');
+        }
+        if (!licenseInput.value) {
+            licenseInput.classList.add('errorClass');
+        }
+        if (!entryInput.value) {
+            entryInput.classList.add('errorClass');
+        }
+        return
+    }
+
+    else {
+        if (vehicleClass.value === 'sedan') {
+            tableRow = document.createElement('tr');
+            carParkSedan.appendChild(tableRow);
+            rowLengthSedan.push(1)
+            if (rowLengthSedan.length >= 10) {
+                alert('car park is filled up')
+                return
+            }
+        } else {
+            tableRow = document.createElement('tr')
+            carParkSuv.appendChild(tableRow)
+            rowLengthSuv.push(1)
+            if (rowLengthSuv.length >= 10) {
+                alert('car park is filled up')
+                return
+            }
+
+        }
+
+        //table data for owner name display
+        const onwerDisplay = document.createElement('td')
+        onwerDisplay.innerText = ownerInput.value
         tableRow.appendChild(onwerDisplay);
-        owner = true;
-    }
 
-    if (!carInput.value) {
-        alert('fill car');
-    } else {
-        const carDisplay = document.createElement('td');
-        carDisplay.innerText = carInput.value
+        //table data for car name display
+        const carDisplay = document.createElement('td')
+        carDisplay.innerText = carInput.value;
         tableRow.appendChild(carDisplay)
-        car = true;
-    }
 
-    if (!licenseInput) {
-        alert('fill license');
-    } else {
+        //table data for license display
         const licenseDisplay = document.createElement('td')
         licenseDisplay.innerText = licenseInput.value;
         tableRow.appendChild(licenseDisplay)
-        license = true
-    }
 
-    if (!entryInput) {
-        alert('fill entry time');
-    } else {
-        const entryDisplay = document.createElement('td');
-        const exitAction = document.createElement('td');
-        const exit = document.createElement('input');
-        exit.setAttribute('type', 'time');
-        const action = document.createElement('input');
-        action.setAttribute('type', 'button');
-        action.value = 'exit'
+        //table data for car entry time display
+        const entryDisplay = document.createElement('td')
         entryDisplay.innerText = entryInput.value;
-        tableRow.appendChild(entryDisplay);
-        tableRow.appendChild(exitAction);
-        exitAction.appendChild(exit);
-        exitAction.appendChild(action);
-        exit.classList.add('exit');
-        action.classList.add('exit');
-        // exit.style.width = '3.5rem';
-        // action.style.width = '3.5rem';
+        tableRow.appendChild(entryDisplay)
+
+        //table data for exit time
+        const exitInput = document.createElement('input')
+        exitInput.setAttribute('type', 'time')
+        tableRow.appendChild(exitInput)
+
+        //create a 'leave' button for vehicles leaving the park.
+        const leaveSec = document.createElement('td')
+        const leaveButton = document.createElement('button')
+        leaveButton.innerText = 'Exit';
+        tableRow.appendChild(leaveSec);
+        leaveSec.appendChild(leaveButton)
+
+        ownerInput.classList.add('cleanClass');
+        carInput.classList.add('cleanClass');
+        licenseInput.classList.add('cleanClass');
+        entryInput.classList.add('cleanClass');
     }
-
-
-
 
     ownerInput.value = '';
     carInput.value = '';
     licenseInput.value = '';
     entryInput.value = '';
-
 
 })
